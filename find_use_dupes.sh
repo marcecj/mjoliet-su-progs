4daaee3e3aa8f0a9b018534674d8b536a3e4d9fe
Marc Joliet <marcec@gmx.de>
2012-10-22 02:58:54 +0200

    Fix make.conf location.

--- find_use_dupes.sh
+++ find_use_dupes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "\n(Potentialy) duplicated USE flags found:\n"
-grep USE= /etc/make.conf | cut -d\" -f2 | tr ' ' '\n' | while read USE_FLAG;
+grep USE= /etc/portage/make.conf | cut -d\" -f2 | tr ' ' '\n' | while read USE_FLAG;
 do
     FINDS="$(grep --color "^[^#].* $USE_FLAG\([[:space:]]\|$\)\{1,\}" /etc/portage/package.use/*[!\~])"
     if [ -n "$FINDS" ]; then
