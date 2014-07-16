0d9b59109a9e3d6fc29b12d0007f3cda930e51f9
Marc Joliet <marcec@gmx.de>
2014-07-16 17:25:08 +0200

    Add argument handling

--- make_backups.sh
+++ make_backups.sh
@@ -4,6 +4,22 @@ DIRS="/
 /home/marcec/multimedia"
 TARGET="/run/media/marcec/MARCEC_BACKUP/"
 
+sync=0
+while getopts s a;
+do
+    case $a in
+        s) sync=1;;
+    esac
+done
+shift $(expr $OPTIND - 1)
+
+prefix="$1"
+count="$2"
+if [ -z "$prefix" -o -z "$count" ]; then
+    echo "Missing arguments!" >&2
+    exit 1
+fi
+
 if [ ! -d "$TARGET" ];
 then
     echo "Non-existent target!"
@@ -17,10 +33,24 @@ do
         exit 1
     fi
 
-    ./btrfs-sync.sh "$d" "$TARGET"
-    echo "******"
+    if [ $sync -eq 1 ]
+    then
+	echo "Syncing source \"$d\" to \"$TARGET\"." >&2
+	./btrfs-sync.sh "$d" "$TARGET"
+
+	if [ $? -ne 0 ]; then
+	    echo "\tError, continuing with next target." >&2
+	    continue
+	fi
+    fi
+
+    target_dir="$(echo ${TARGET}$(hostname)${d} | sed s:/$::g)"
+
+    btrfs-snap -r "$target_dir" "$prefix" "$count"
 
     if [ $? -ne 0 ]; then
-	echo "Error syncing source \"$d\" to \"$TARGET\", continuing with next target." >&2
+	echo "Error running btrfs-snap on \"$target_dir\"." >&2
     fi
+
+    echo "******"
 done
