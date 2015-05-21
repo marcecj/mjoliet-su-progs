014552d8bce77329961bf97bb9255b47670df4d7
Marc Joliet <marcec@gmx.de>
2015-05-21 14:55:04 +0200

    make_backups2.sh: fix indentation

--- make_backups2.sh
+++ make_backups2.sh
@@ -18,24 +18,24 @@ init_target_subvolumes() {
 
     echo "$SOURCES" | while read d;
     do
-	local tgt="${TARGET}${d}"
+        local tgt="${TARGET}${d}"
 
-	if $ssh [ ! -d "$tgt" ]; then
-	    # Create the parent directory of the source subvolume, because
-	    # "btrfs subvolume create" will not create it automatically.
-	    local tgt_snap_parent=$(echo "$tgt" | sed -e "s:\(.*\)/\([^/]\+/\?\):\1:g")
-	    if $ssh [ ! -d "$tgt_snap_parent" ]; then
-		$ssh mkdir -p "$tgt_snap_parent"
-		echo "INFO: Created source subvolume parent \"$tgt_snap_parent\"."
-	    fi
+        if $ssh [ ! -d "$tgt" ]; then
+            # Create the parent directory of the source subvolume, because
+            # "btrfs subvolume create" will not create it automatically.
+            local tgt_snap_parent=$(echo "$tgt" | sed -e "s:\(.*\)/\([^/]\+/\?\):\1:g")
+            if $ssh [ ! -d "$tgt_snap_parent" ]; then
+                $ssh mkdir -p "$tgt_snap_parent"
+                echo "INFO: Created source subvolume parent \"$tgt_snap_parent\"."
+            fi
 
-	    $ssh btrfs subvolume create "$tgt"
-	fi
+            $ssh btrfs subvolume create "$tgt"
+        fi
 
-	if $ssh [ ! -d "$tgt/.snapshot" ]; then
-	    $ssh mkdir "$tgt/.snapshot"
-	    echo "INFO: Created snapshot directory \""$tgt"/.snapshot\"."
-	fi
+        if $ssh [ ! -d "$tgt/.snapshot" ]; then
+            $ssh mkdir "$tgt/.snapshot"
+            echo "INFO: Created snapshot directory \""$tgt"/.snapshot\"."
+        fi
     done
 }
 
@@ -45,8 +45,8 @@ init=0
 while getopts r:i a;
 do
     case $a in
-	r) ssh="ssh -n $OPTARG";
-	   ssh_pipe="ssh $OPTARG";;
+        r) ssh="ssh -n $OPTARG";
+           ssh_pipe="ssh $OPTARG";;
         i) init=1;;
     esac
 done
@@ -84,9 +84,9 @@ transfer_subvolume() {
     echo "LOG: Transferring snapshot of '$src' to '$tgt'"
 
     if [ "$num_snapshots" -ge 2 ]; then
-	btrfs send -p "$parent_snapshot" "$current_snapshot" | $ssh_pipe btrfs receive "$tgt"
+        btrfs send -p "$parent_snapshot" "$current_snapshot" | $ssh_pipe btrfs receive "$tgt"
     else
-	btrfs send "$current_snapshot" | $ssh_pipe btrfs receive "$tgt"
+        btrfs send "$current_snapshot" | $ssh_pipe btrfs receive "$tgt"
     fi
 }
 
@@ -97,23 +97,23 @@ rotate_prefix() {
     local old_prefix=""
 
     case "$prefix" in
-	"hourly") return;;
-	"daily") old_prefix="hourly";;
-	"weekly") old_prefix="daily";;
-	"monthly") old_prefix="weekly";;
+        "hourly") return;;
+        "daily") old_prefix="hourly";;
+        "weekly") old_prefix="daily";;
+        "monthly") old_prefix="weekly";;
     esac
 
     local old_snapshot="$($ssh ls -1d ${tgt}/${old_prefix}* | head -n1)"
     local new_snapshot=$(echo "$old_snapshot" | sed -e "s:${old_prefix}_\([^/]\+\):${prefix}_\1:g")
 
     if $ssh [ -z "$old_snapshot" ]; then
-	echo "INFO: No snapshots to rotate."
-	return
+        echo "INFO: No snapshots to rotate."
+        return
     fi
 
     if $ssh [ -d "$new_snapshot" ]; then
-	echo "INFO: already made daily snapshot."
-	return
+        echo "INFO: already made daily snapshot."
+        return
     fi
 
     $ssh btrfs subvolume snapshot -r "$old_snapshot" "$new_snapshot"
@@ -127,10 +127,10 @@ del_oldest_snapshot() {
     local oldest_snapshots="$($ssh ls -1d $tgt/${prefix}* | head -n$num_to_delete)"
 
     if [ "$num_snapshots" -gt "$count" ]; then
-	echo "LOG: deleting oldest snapshot."
-	echo "$oldest_snapshots" | while read s; do
-	    $ssh btrfs subvolume delete "$s"
-	done
+        echo "LOG: deleting oldest snapshot."
+        echo "$oldest_snapshots" | while read s; do
+            $ssh btrfs subvolume delete "$s"
+        done
     fi
 }
 
@@ -141,7 +141,7 @@ del_current_snapshot() {
     local current_snapshot="$(ls -1d $src/.snapshot/${prefix}* | tail -n1)"
 
     if [ "$num_snapshots" -ge 2 ]; then
-	btrfs subvolume delete "$current_snapshot"
+        btrfs subvolume delete "$current_snapshot"
     fi
 }
 
@@ -160,27 +160,27 @@ do
 
     if [ "$prefix" = "hourly" ];
     then
-	# make a snapshot of the source volume; we only need to keep two around
-	# for incremental send/receive
-	btrfs-snap -r "$d" "$prefix" 2
+        # make a snapshot of the source volume; we only need to keep two around
+        # for incremental send/receive
+        btrfs-snap -r "$d" "$prefix" 2
 
-	if [ $? -ne 0 ]; then
-	    echo "\tError creating snapshot." >&2
-	    exit 4
-	fi
+        if [ $? -ne 0 ]; then
+            echo "\tError creating snapshot." >&2
+            exit 4
+        fi
 
-	transfer_subvolume "$d" "$tgt"
+        transfer_subvolume "$d" "$tgt"
 
-	if [ $? -ne 0 ]; then
-	    echo "\tError transferring, continuing with next target." >&2
-	    echo "" >&2
-	    echo "\tTo prevent breaking the chain of snapshots," >&2
-	    echo "\tthe most recent one will be deleted." >&2
+        if [ $? -ne 0 ]; then
+            echo "\tError transferring, continuing with next target." >&2
+            echo "" >&2
+            echo "\tTo prevent breaking the chain of snapshots," >&2
+            echo "\tthe most recent one will be deleted." >&2
 
-	    del_current_snapshot "$d"
+            del_current_snapshot "$d"
 
-	    continue
-	fi
+            continue
+        fi
     fi
 
     rotate_prefix "$tgt"
