0d0258ba1608993af78c48a785fae521c92d1254
Marc Joliet <marcec@gmx.de>
2018-01-03 03:28:10 +0100

    init_gentoo_container.sh: use config directories
    Rework init_gentoo_container.sh to use a configuration repository with
    one directory per container (which is, however, optional).  This is to
    make the script more suitable for creating container images for testing
    other packages.
    
    The way this works now is as follows (with ${CONFIG_DIR} as the
    container's configuration directory):
    
    1.) The container image is downloaded.
    2.) An optional systemd nswap unit is installed on the host.
    3.) systemd-nspawn runs a series of commands in the container (with
      ${CONFIG_DIR} bind mounted therein as /root/container.conf/):
        3a.) Files in filesystem/ are installed into the root file system.
        3b.) The portage tree is initialised via emerge-webrsync followed by
        "emerge --sync".
        3c.) All files in scripts.preinst/ ending in .sh are executed.
        3d.) A @world update is performed.
        3e.) Packages listed in "packages" are installed in @world.
        3f.) All files in scripts.postinst/ ending in .sh are executed.
    
    Steps 2, 3a, 3c, 3e, and 3f are all optional.  Also note that the
    container is run with user namespace support active (i.e., it uses the
    "-U" option to systemd-nspawn).  Finally, the *.sh files must be shell
    scripts.

--- init_gentoo_container.sh
+++ init_gentoo_container.sh
@@ -3,16 +3,21 @@
 arch="amd64"
 stage3_variant="-amd64-systemd"
 container_name="gentoo-amd64-systemd"
-while getopts a:s:n: a
+config_base_dir="${XDG_CONFIG_HOME:-$HOME/.config}/gentoo_containers"
+
+while getopts a:d:s:n: a
 do
     case $a in
         a) arch="$OPTARG";;
+        d) if [ ! -d "$OPTARG" ]; then echo >&2 "Error: path \"$OPTARG\" does not exist."; exit 1; fi
+	config_base_dir="$OPTARG";;
         s) stage3_variant="-$OPTARG";;
         n) container_name="$OPTARG";;
         \?) exit 1;;
     esac
 done
 
+CONFIG_DIR="${config_base_dir}/${container_name}"
 BASE_URL="http://distfiles.gentoo.org/releases/${arch}/autobuilds/"
 LATEST="${BASE_URL}latest-stage3${stage3_variant}.txt"
 STAGE3_URL="${BASE_URL}$(curl -s $LATEST | tail -n1 | cut -d' ' -f1)"
@@ -20,82 +25,57 @@ STAGE3_URL="${BASE_URL}$(curl -s $LATEST | tail -n1 | cut -d' ' -f1)"
 # create the initial container from the current stage3 image
 machinectl pull-tar --verify=no "${STAGE3_URL}" "${container_name}"
 
-# make sure the machine mounts the systems distfiles as a read-only FS
-cat <<EOF > "/etc/systemd/nspawn/${container_name}.nspawn"
-[Files]
-BindReadOnly=/home/marcec/projects/gentoo/:/home/marcec/gentoo/
-BindReadOnly=/usr/portage/distfiles/:/usr/portage/ro_distfiles/
-TemporaryFileSystem=/var/tmp/portage/
+# install an nspawn file if available
+if [ -f "${CONFIG_DIR}/container.nspawn" ]
+then
+    install -m0644 \
+        "${CONFIG_DIR}/container.nspawn" \
+        "/etc/systemd/nspawn/${container_name}.nspawn"
+fi
+
+# systemd-nspawn will use a temporary source directory if $CONFIG_DIR is empty,
+# so make sure it is if $CONFIG_DIR does not exist
+[ -d "$CONFIG_DIR" ] || CONFIG_DIR=""
+
+systemd-nspawn -U -M "${container_name}" --bind-ro="${CONFIG_DIR}":/root/container.conf/ sh -s <<EOF
+
+# install custom files into the file system
+if [ -d /root/container.conf/filesystem ]
+then
+    printf "\n*** Copying files into the root file system.\n"
+    rsync -r /root/container.conf/filesystem/ /
+fi
+
+# initialise and sync the portage tree
+emerge-webrsync
+emerge --sync
+
+# run pre-installation setup
+if [ -d /root/container.conf/scripts.preinst ]
+then
+    for f in /root/container.conf/scripts.preinst/*.sh
+    do
+        printf "\n*** Executing \"\${f}\" in container\n"
+        . "\${f}"
+    done
+fi
+
+# update the system
+emerge @world -uDNv
+
+# install desired packages
+if [ -f /root/container.conf/packages ]
+then
+    emerge -v \$(cat /root/container.conf/packages)
+fi
+
+# run post-installation setup
+if [ -d /root/container.conf/scripts.postinst ]
+then
+    for f in /root/container.conf/scripts.postinst/*.sh
+    do
+        printf "\n*** Executing \"\${f}\" in container\n"
+        . "\${f}"
+    done
+fi
 EOF
-
-### configure portage
-
-host_cpu_flags="$(grep CPU_FLAGS /etc/portage/make.conf)"
-systemd-nspawn -M "${container_name}" sh -c "
-mkdir \
-          /etc/portage/env \
-          /etc/portage/package.accept_keywords \
-          /etc/portage/package.env \
-          /etc/portage/package.use \
-          /etc/portage/profile
-
-echo 'app-text/asciidoc -python_targets_pypy -python_single_target_pypy' > /etc/portage/profile/package.use.mask
-echo 'FEATURES=\"\${FEATURES} test\"' > /etc/portage/env/test.conf
-echo 'app-text/asciidoc test.conf' > /etc/portage/package.env/test
-
-cat << EOF > /etc/portage/package.accept_keywords/asciidoc
-app-text/asciidoc **
-~dev-python/pypy-bin-5.9.0 ~amd64
-~virtual/pypy-5.9.0 ~amd64
-EOF
-
-cat << EOF > /etc/portage/package.use/asciidoc
-# app-text/asciidoc python_targets_pypy python_single_target_pypy -python_single_target_python2_7
-app-text/asciidoc python_targets_pypy
-
-# required by media-sound/lilypond-2.18.2::gentoo
-# required by app-text/asciidoc-8.6.9-r1::gentoo[test]
-# required by asciidoc (argument)
->=media-gfx/fontforge-20150612-r1 png
-# required by media-gfx/graphviz-2.38.0-r1::gentoo
-# required by app-text/asciidoc-8.6.9-r1::gentoo[test]
-# required by asciidoc (argument)
->=media-libs/gd-2.1.1 truetype png fontconfig jpeg
-
-# fetching from github requires curl support in git
-dev-vcs/git curl
-
-# required by app-text/texlive-core-2014-r4::gentoo[xetex]
-# required by dev-texlive/texlive-mathextra-2014::gentoo
-# required by app-text/dblatex-0.3.7::gentoo
-# required by dblatex (argument)
->=media-libs/harfbuzz-0.9.41 icu
-# required by dev-texlive/texlive-xetex-2014::gentoo
-# required by app-text/dblatex-0.3.7::gentoo
-# required by dblatex (argument)
->=app-text/texlive-core-2014-r4 xetex
-EOF
-
-sed -i s:USE=.\*:USE=\\\"-X\\\":g /etc/portage/make.conf
-echo '${host_cpu_flags}' >> /etc/portage/make.conf
-
-cat << EOF >> /etc/portage/make.conf
-PORTAGE_RO_DISTDIRS=\"\\\${PORTDIR}/ro_distfiles\"
-PORTAGE_RSYNC_EXTRA_OPTS=\"--exclude=/ro_distfiles\"
-EMERGE_DEFAULT_OPTS=\"--with-bdeps=y --quiet-build=y --nospinner --jobs 2 --load-average 4 --keep-going\"
-EOF
-"
-
-### update the system and install desired packages
-
-systemd-nspawn -M "${container_name}" emerge-webrsync
-systemd-nspawn -M "${container_name}" emerge --sync
-systemd-nspawn -M "${container_name}" emerge @world -uDNv
-systemd-nspawn -M "${container_name}" emerge -v \
-        app-editors/vim \
-        app-portage/eix \
-        app-portage/gentoolkit \
-        app-text/asciidoc \
-        dev-vcs/git \
-        virtual/pypy
-systemd-nspawn -M "${container_name}" eix-update
